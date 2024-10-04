a2cd443f26aa0f55e4ff70c3ea92b176ce149482
Lebohang <lebomdlongwa@gmail.com>
2024-10-04 16:24:53 +0200

    report item expands when user clicks delete or edit button

--- assets/js/frontend/containers/ReportList/report/report.tsx
+++ assets/js/frontend/containers/ReportList/report/report.tsx
@@ -3,8 +3,6 @@ import * as styled from "./styles/report";
 
 import { EditIcon } from "../../../components/icons/edit";
 import { TrashIcon } from "../../../components/icons/trash";
-import { SearchIcon } from "../../../components/icons/search";
-import { Color } from "../../../components/colorCodes";
 import { SpeakerIcon } from "../../../components/icons/speaker";
 import { GradeIcon } from "../../../components/icons/grade";
 import { CalendarIcon } from "../../../components/icons/calendar";
@@ -22,44 +20,46 @@ export const Report = () => {
 
   return (
     <styled.ReportWrapper>
-      <styled.ReportBody onClick={handleExpand}>
+      <styled.ReportBody>
         <styled.ReportItemWrapper>
           <styled.ReportItem>
-            <styled.Informant>
-              <styled.Icon>
-                <SpeakerIcon h={20} w={20} />
-                <styled.IconLabel>INFORMANT</styled.IconLabel>
-              </styled.Icon>
-              <styled.Content>BUKHOSI NDLELA</styled.Content>
-            </styled.Informant>
-            <styled.CaseNumber>
-              <styled.Icon>
-                <InfoIcon h={18} w={18} />
-                <styled.IconLabel>CASE No.</styled.IconLabel>
-              </styled.Icon>
-              <styled.Content>76873-J</styled.Content>
-            </styled.CaseNumber>
-            <styled.Officer>
-              <styled.Icon>
-                <PolicemanIcon h={21} w={21} />
-                <styled.IconLabel>Officer</styled.IconLabel>
-              </styled.Icon>
-              <styled.Content>B CHIKOVORE</styled.Content>
-            </styled.Officer>
-            <styled.ReportGrade>
-              <styled.Icon>
-                <GradeIcon h={19} w={19} />
-                <styled.IconLabel>GRADE</styled.IconLabel>
-              </styled.Icon>
-              <styled.Content>A</styled.Content>
-            </styled.ReportGrade>
-            <styled.ReportDate>
-              <styled.Icon>
-                <CalendarIcon h={15} w={15} />
-                <styled.IconLabel>REPORT DATE</styled.IconLabel>
-              </styled.Icon>
-              <styled.Content>27-10-01</styled.Content>
-            </styled.ReportDate>
+            <styled.ReportContent onClick={handleExpand}>
+              <styled.Informant>
+                <styled.Icon>
+                  <SpeakerIcon h={20} w={20} />
+                  <styled.IconLabel>INFORMANT</styled.IconLabel>
+                </styled.Icon>
+                <styled.Content>BUKHOSI NDLELA</styled.Content>
+              </styled.Informant>
+              <styled.CaseNumber>
+                <styled.Icon>
+                  <InfoIcon h={18} w={18} />
+                  <styled.IconLabel>CASE No.</styled.IconLabel>
+                </styled.Icon>
+                <styled.Content>76873-J</styled.Content>
+              </styled.CaseNumber>
+              <styled.Officer>
+                <styled.Icon>
+                  <PolicemanIcon h={21} w={21} />
+                  <styled.IconLabel>Officer</styled.IconLabel>
+                </styled.Icon>
+                <styled.Content>B CHIKOVORE</styled.Content>
+              </styled.Officer>
+              <styled.ReportGrade>
+                <styled.Icon>
+                  <GradeIcon h={19} w={19} />
+                  <styled.IconLabel>GRADE</styled.IconLabel>
+                </styled.Icon>
+                <styled.Content>A</styled.Content>
+              </styled.ReportGrade>
+              <styled.ReportDate>
+                <styled.Icon>
+                  <CalendarIcon h={15} w={15} />
+                  <styled.IconLabel>REPORT DATE</styled.IconLabel>
+                </styled.Icon>
+                <styled.Content>27-10-01</styled.Content>
+              </styled.ReportDate>
+            </styled.ReportContent>
             <styled.ButtonsWrapper>
               <styled.Button>
                 <EditIcon h={24} w={24} />
