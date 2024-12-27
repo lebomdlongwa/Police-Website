d631e84485071d6c89bda5b9768ffd155f7eccfb
Lebohang <lebomdlongwa@gmail.com>
2024-12-27 17:12:09 +0200

    remove scroll bars and toggle Person Details

--- assets/js/frontend/containers/Chat/chatApp.tsx
+++ assets/js/frontend/containers/Chat/chatApp.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styled from "./styles/chatApp";
 import { ChatSideBar } from "./ChatSideBar";
 import { ChatBody } from "./ChatBody/chatBody";
 import { ChatEndBar } from "./ChatEndBar/endBar";
 
 export const ChatAppComponent = () => {
+  const [showPersonDetails, setShowPersonDetails] = useState(true);
+
+  const handleShowPersonDetails = () => setShowPersonDetails(false);
+
   return (
     <styled.ChatAppWrapper>
       <ChatSideBar />
       <ChatBody />
-      <ChatEndBar />
+      {showPersonDetails && (
+        <ChatEndBar ShowPersonDetails={handleShowPersonDetails} />
+      )}
     </styled.ChatAppWrapper>
   );
 };
